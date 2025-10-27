02f6131e7a9fffa730813ce42daae025e3a5c5ad
anirs <imran3@gmail.com>
2025-10-27 12:14:54 -0400

    Update ble_adv_controller.cpp

--- components/ble_adv_controller/ble_adv_controller.cpp
+++ components/ble_adv_controller/ble_adv_controller.cpp
@@ -15,7 +15,7 @@ void BleAdvSelect::control(const std::string &value) {
 }
 
 void BleAdvSelect::sub_init() { 
-  App.register_select(this);
+  //App.register_select(this);
   this->rtc_ = global_preferences->make_preference< uint32_t >(this->get_object_id_hash());
   uint32_t restored;
   if (this->rtc_.load(&restored)) {
@@ -34,7 +34,7 @@ void BleAdvNumber::control(float value) {
 }
 
 void BleAdvNumber::sub_init() {
-  App.register_number(this);
+  //App.register_number(this);
   this->rtc_ = global_preferences->make_preference< float >(this->get_object_id_hash());
   float restored;
   if (this->rtc_.load(&restored)) {
